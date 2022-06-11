8fae648ab0a562bbadff67515a3d06724736e092
fayez-baig <coderninja16@gmail.com>
2022-06-11 21:06:13 +0530

    feat(🚸 ): added stop and run button

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -1,10 +1,24 @@
 import React from "react";
 import CatSprite from "./CatSprite";
+import Flag from "./Flag";
 
 const PreviewArea = () => {
   return (
     <div className="flex-none h-full p-2 overflow-y-auto">
       <CatSprite />
+
+      <button
+        className="fixed flex items-center justify-center w-12 h-12 rounded-full right-5 bottom-5 text-gray-50"
+        data-run
+      >
+        <Flag />
+      </button>
+      <button
+        className="fixed w-12 h-12 bg-red-500 rounded-full right-20 bottom-5 hover:bg-red-700 text-gray-50"
+        data-reset
+      >
+        Reset
+      </button>
     </div>
   );
 };
